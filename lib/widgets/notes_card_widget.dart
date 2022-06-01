050cdd0b9a6ffc8fef9fcb96e902f24d9119bf71
KevinBenaya <darkfaust063@gmail.com>
2022-06-01 18:37:32 +0700

    Adding notes_card_widget.dart

---
+++ lib/widgets/notes_card_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kelasku_tubes/model/models.dart';
+
+class NotesCard extends StatelessWidget {
+  final Note note;
+
+  const NotesCard(this.note);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      padding: const EdgeInsets.all(15),
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            note.judul,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            note.deskripsi,
+            style: const TextStyle(fontSize: 17),
+          ),
+        ],
+      ),
+    );
+  }
+}
