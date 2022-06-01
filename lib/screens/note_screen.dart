a52f0951232a602c9fcf2352fa54f8f0060d7ded
KevinBenaya <darkfaust063@gmail.com>
2022-06-01 18:20:48 +0700

    Adding new screen(note_screen.dart)

---
+++ lib/screens/note_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class NoteScreen extends StatelessWidget {
+  const NoteScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add, size: 30, color: Colors.blueGrey),
+        backgroundColor: Colors.white,
+      ),
+      appBar: AppBar(
+        title: const Text(
+          'KelasKu',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.4,
+        child: Consumer<NotesOperation>(
+          builder: (context, NotesOperation data, child) {
+            return ListView.builder(
+              itemCount: data.getNotes.length,
+              itemBuilder: (context, index) {
+                return NotesCard(data.getNotes[index]);
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
