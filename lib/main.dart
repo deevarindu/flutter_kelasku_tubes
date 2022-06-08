394374b6325466fdc0e85289ff027df2b1b5fecf
KevinBenaya <darkfaust063@gmail.com>
2022-06-08 15:03:41 +0700

    Adding Provider.debugCheckInvalidValueType

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_kelasku_tubes/app.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  Provider.debugCheckInvalidValueType = null;
   runApp(
     const App(),
   );
