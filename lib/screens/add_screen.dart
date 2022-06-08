b3472d2f894476c35f943b86680728dba1f4e2e9
KevinBenaya <darkfaust063@gmail.com>
2022-06-08 15:03:14 +0700

    Fixing Provider errors

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -10,85 +10,88 @@ class AddScreen extends StatelessWidget {
     String? judulTeks;
     String? deskripsiTeks;
 
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: const Text('KelasKu'),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.4,
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 15,
-            left: 15,
-            right: 15,
-            bottom: 80,
-          ),
-          child: Column(
-            children: [
-              TextField(
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Judul',
-                  hintStyle: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                onChanged: (value) {
-                  judulTeks = value;
-                },
-              ),
-              Expanded(
-                child: TextField(
+    return Provider<NotesOperation>(
+      create: (BuildContext context) => NotesOperation(),
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('KelasKu'),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              left: 15,
+              right: 15,
+              bottom: 80,
+            ),
+            child: Column(
+              children: [
+                TextField(
                   decoration: const InputDecoration(
                     border: InputBorder.none,
-                    hintText: 'Enter Description',
+                    hintText: 'Judul',
                     hintStyle: TextStyle(
-                      fontSize: 18,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                       color: Colors.white,
                     ),
                   ),
                   style: const TextStyle(
-                    fontSize: 18,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                     color: Colors.white,
                   ),
                   onChanged: (value) {
-                    deskripsiTeks = value;
+                    judulTeks = value;
                   },
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Provider.of<NotesOperation>(context, listen: false)
-                      .addNewNote(judulTeks!, deskripsiTeks!);
-                  Navigator.pop(context);
-                },
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(
-                      left: 30, right: 30, top: 10, bottom: 10),
-                  backgroundColor: Colors.white,
-                ),
-                child: const Text(
-                  'Tambahkan Catatan',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blueGrey,
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter Description',
+                      hintStyle: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                    onChanged: (value) {
+                      deskripsiTeks = value;
+                    },
                   ),
                 ),
-              )
-            ],
+                TextButton(
+                  onPressed: () {
+                    Provider.of<NotesOperation>(context, listen: false)
+                        .addNewNote(judulTeks!, deskripsiTeks!);
+                    Navigator.pop(context);
+                  },
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.only(
+                        left: 30, right: 30, top: 10, bottom: 10),
+                    backgroundColor: Colors.white,
+                  ),
+                  child: const Text(
+                    'Tambahkan Catatan',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -9,44 +9,47 @@ class NoteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const AddScreen(),
-            ),
-          );
-        },
-        child: const Icon(Icons.add, size: 30, color: Colors.blueGrey),
-        backgroundColor: Colors.white,
-      ),
-      appBar: AppBar(
-        title: const Text(
-          'KelasKu',
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.4,
-        child: Consumer<NotesOperation>(
-          builder: (context, NotesOperation data, child) {
-            return ListView.builder(
-              itemCount: data.getNotes.length,
-              itemBuilder: (context, index) {
-                return NotesCard(data.getNotes[index]);
-              },
+    return Provider<NotesOperation>(
+      create: (BuildContext context) => NotesOperation(),
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const AddScreen(),
+              ),
             );
           },
+          child: const Icon(Icons.add, size: 30, color: Colors.blueGrey),
+          backgroundColor: Colors.white,
+        ),
+        appBar: AppBar(
+          title: const Text(
+            'KelasKu',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: Consumer<NotesOperation>(
+            builder: (context, NotesOperation data, child) {
+              return ListView.builder(
+                itemCount: data.getNotes.length,
+                itemBuilder: (context, index) {
+                  return NotesCard(data.getNotes[index]);
+                },
+              );
+            },
+          ),
         ),
       ),
     );
