c8adf82316723c20b58ef1a779d8a6a522467eb6
KevinBenaya <darkfaust063@gmail.com>
2022-06-14 10:35:27 +0700

    For Fixing note

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kelasku_tubes/app.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_kelasku_tubes/model/models.dart';
 
 void main() {
   Provider.debugCheckInvalidValueType = null;
-  runApp(
-    const App(),
-  );
+  runApp(const App());
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<NotesOperation>(
+      create: (context) => NotesOperation(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
 }
