3c0bda484205fb677738b8434f4b5452d4d43906
KevinBenaya <darkfaust063@gmail.com>
2022-06-16 12:06:26 +0700

    Gridview Widgets

---
+++ lib/widgets/custom_card_mapel_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({Key? key, required this.judul, required this.gambar})
+      : super(key: key);
+
+  final String? judul;
+  final String? gambar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Card(
+        //menambahkan bayangan
+        elevation: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                      gambar!,
+                    ),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(judul!),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
