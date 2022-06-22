93f1e8f32f3db60a2c0d32fb3250170c64c52bac
KevinBenaya <darkfaust063@gmail.com>
2022-06-22 12:26:11 +0700

    Export kelas 8

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -4,5 +4,5 @@ export 'home_screen.dart';
 export 'profile_screen.dart';
 export 'mapel_detail_screen.dart';
 export 'add_screen.dart';
-export 'bahasa_indonesia_screen.dart';
-export 'matematika_screen.dart';
+export 'bab_indonesia_screen7.dart';
+export 'bab_indonesia_screen8.dart';
