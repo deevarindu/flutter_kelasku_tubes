8f6ef3d1a56e7f9421e99e671f153c0b191e19dd
KevinBenaya <darkfaust063@gmail.com>
2022-06-22 12:16:01 +0700

    Bab kelas 7 screen

---
+++ lib/screens/bab_indonesia_screen7.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_kelasku_tubes/model/models.dart';
+
+class BahasaIndonesiaBab7 extends StatelessWidget {
+  const BahasaIndonesiaBab7({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: AppBar(
+        title: const Text('Bahasa Indonesia'),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(20),
+        itemCount: babList7.length,
+        itemBuilder: (context, index) {
+          final Bab7 bab7 = babList7[index];
+          return Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Text(bab7.bab),
+              ),
+              title: Text(bab7.judul),
+              subtitle: Text(bab7.subJudul),
+              trailing:
+                  IconButton(icon: Icon(Icons.chevron_right), onPressed: () {}),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
