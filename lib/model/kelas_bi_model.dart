bc9766e87b6fee67d4d20b8923c33ca0cfd79703
KevinBenaya <darkfaust063@gmail.com>
2022-06-22 13:33:37 +0700

    Kelas BI model

---
+++ lib/model/kelas_bi_model.dart
@@ -0,0 +1,24 @@
+class KelasBI {
+  String title;
+  String image;
+
+  KelasBI({
+    required this.title,
+    required this.image,
+  });
+}
+
+var KelasBIList = [
+  KelasBI(
+    title: 'Kelas 7',
+    image: 'assets/images/mapel/bindo.png',
+  ),
+  KelasBI(
+    title: 'Kelas 8',
+    image: 'assets/images/mapel/bindo.png',
+  ),
+  KelasBI(
+    title: 'Kelas 9',
+    image: 'assets/images/mapel/bindo.png',
+  ),
+];
