965f3602f41c6c84cb27c4403d1bbbc352a058f9
KevinBenaya <darkfaust063@gmail.com>
2022-06-22 12:49:48 +0700

    Tambah kelas 9 mat + model

---
+++ lib/model/bab_data_mat_kelas9.dart
@@ -0,0 +1,44 @@
+class Bab9Mat {
+  String judul;
+  String bab;
+  String subJudul;
+
+  Bab9Mat({
+    required this.judul,
+    required this.bab,
+    required this.subJudul,
+  });
+}
+
+var babList9Mat = [
+  Bab9Mat(
+    bab: 'I',
+    judul: 'Bab 1',
+    subJudul: 'Kesebangunan',
+  ),
+  Bab9Mat(
+    bab: 'II',
+    judul: 'Bab 2',
+    subJudul: 'Bangun Ruang Sisi Lengkung',
+  ),
+  Bab9Mat(
+    bab: 'III',
+    judul: 'Bab 3',
+    subJudul: 'Persamaan Garis Lurus',
+  ),
+  Bab9Mat(
+    bab: 'IV',
+    judul: 'Bab 4',
+    subJudul: 'Pengolahan dan Penyajian Data',
+  ),
+  Bab9Mat(
+    bab: 'V',
+    judul: 'Bab 5',
+    subJudul: 'Bilangan Berpangkat dan Bentuk Akar',
+  ),
+  Bab9Mat(
+    bab: 'VI',
+    judul: 'Bab 6',
+    subJudul: 'Barisan dan Deret',
+  ),
+];
--- lib/model/models.dart
+++ lib/model/models.dart
@@ -3,3 +3,6 @@ export 'notes_operation.dart';
 export 'bab_data_bi_kelas7.dart';
 export 'bab_data_bi_kelas8.dart';
 export 'bab_data_bi_kelas9.dart';
+export 'bab_data_mat_kelas7.dart';
+export 'bab_data_mat_kelas8.dart';
+export 'bab_data_mat_kelas9.dart';
